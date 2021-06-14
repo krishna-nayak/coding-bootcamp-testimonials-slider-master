467ca2761fd41783fb13cb89ca3b0c924dd34a7a
Krishna Nayak <nayak100krishna@gmail.com>
2021-06-14 22:37:11 +0530

    style-desktop

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const data = [
     {
         name: `Tanya Sinclair`,
         description: `“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”`,
-        profection: `UX Engineer`,
+        job: `UX Engineer`,
     },
     {
         name: `John Tarkpor`,
         description: `“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”`,
-        profection: `Junior Front-end Developer`,
+        job: `Junior Front-end Developer`,
     },
 ];
